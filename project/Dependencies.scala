a7218cb9aa574415de6bb1fdd83ea9fc87bf65cf
Seeta Ramayya Vadali <srvadali@gmail.com>
2021-09-13 05:48:42 +0200

    First few spark lines (dataframe)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,14 +1,18 @@
 import sbt._
 
 object Dependencies {
+  private val sparkVersion = "3.1.2"
 
   // https://github.com/apache/spark
-  lazy private val spark = "org.apache.spark" %% "spark-core" % "3.1.2"
+  lazy private val sparkCore = "org.apache.spark" %% "spark-core" % sparkVersion
+
+  // https://github.com/apache/spark
+  lazy private val sparkSql = "org.apache.spark" %% "spark-sql" % sparkVersion
 
   // https://github.com/scalatest/scalatest
   lazy private val scalaTest = "org.scalatest" %% "scalatest" % "3.2.9" % "test"
 
-  lazy private val compileDependencies: Seq[ModuleID] = Seq(spark)
+  lazy private val compileDependencies: Seq[ModuleID] = Seq(sparkCore, sparkSql)
 
   lazy private val testDependencies: Seq[ModuleID] = Seq(scalaTest)
 
--- src/main/scala/learn/spark/HelloWorld.scala
+++ src/main/scala/learn/spark/HelloWorld.scala
@@ -1,5 +1,22 @@
 package learn.spark
 
+import org.apache.spark.sql.SparkSession
+
 object HelloWorld extends App {
+  val fileName = "src/main/resources/data/cars.json"
+  val spark = SparkSession.builder()
+    .appName("hello-world")
+    .config("spark.master", "local")
+    .getOrCreate()
+
+  val dataFrame = spark.read
+    .format("json")
+    .option("inferSchema", "true") // automatically infer the schema but inferring might be wrong, see Year inferred as String
+    .load(fileName)
+
+  dataFrame.show()
+  dataFrame.printSchema()
+
+  dataFrame.take(10).foreach(println)
 
 }
